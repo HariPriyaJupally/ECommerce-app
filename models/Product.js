cbc10c808ad2600c65a6e727d20b5fc1e23868aa
Vamsee Krishna Gangapatnam <crvamsee@gmail.com>
2018-11-13 19:35:46 -0600

    Updated product in model and data

---
+++ models/Product.js
@@ -0,0 +1,35 @@
+/**
+ * Product schema to store all product details by using mongoose 
+ * @author Gangapatnam, Vamse Krishna
+ */
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+
+  _productid: { type: Number, required: true },
+  productName: {
+    type: String,
+    required: true,
+    default: 'DELL-LAPTOP'
+  }, 
+  productDescription: {
+    type: String,
+    required: true,
+    default: 'TOP brabded Laptop'
+  }, 
+  price: {
+    type: Number,
+    required: false,
+    default:1000
+  },
+  productCategory: { 
+    type: String,
+    required: true,
+    default: 'Laptops'
+  },
+  sellerId: { 
+    type: Number,
+    required: true,
+  }
+})
+module.exports = mongoose.model('Product', ProductSchema)
