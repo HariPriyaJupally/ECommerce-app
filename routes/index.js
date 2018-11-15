b82bd72439abe549179dc5789a9cc5f13164c341
Hayden Rainey <s513768@nwmissouri.edu>
2018-11-15 13:12:52 -0600

    product router to point to orderline instead. accidentally pointed to wrong controller last time.

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/', (req, res, next) => {
 
 // Defer path requests to a particular controller
 router.use('/about', require('../controllers/about.js'))
-router.use('/product', require('../controllers/product.js'))
+router.use('/orderLine', require('../controllers/orderLine.js'))
 
 LOG.debug('END routing')
 module.exports = router
